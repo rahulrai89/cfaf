8e777c8cb526a647e27c4d24adbbf284339274f5
Rahul Rai <hcsrahul.rai@gmail.com>
2024-03-25 03:11:56 +0530

    Added Gulp Files

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+// Import the required modules
+const { src, dest, series, parallel } = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
+const csso = require('gulp-csso');
+const del = require('del');
+const sass = require('gulp-sass')(require('sass'));
+const uglify = require('gulp-uglify');
+
+// Gulp task to minify CSS files
+function styles() {
+  return src('src/sass/styles.scss')
+    // Compile SASS files
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    // Auto-prefix css styles for cross browser compatibility
+    .pipe(autoprefixer())
+    // Minify the file
+    .pipe(csso())
+    // Output
+    .pipe(dest('dist/css'));
+}
+
+// Gulp task to minify JavaScript files
+function scripts() {
+  return src('src/js/**/*.js')
+    // Minify the file
+    .pipe(uglify())
+    // Output
+    .pipe(dest('dist/js'));
+}
+
+// Clean output directory
+function clean() {
+  return del(['dist']);
+}
+
+// Gulp task to minify all files
+exports.default = series(clean, parallel(styles, scripts));
